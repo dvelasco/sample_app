b48ee04d85344ded3e7e4562bca73ce0c1b106b8
dvelasco <dvelasco@cranium-solutions.com>
2011-02-18 14:15:35 -0800

    Added user following

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,6 +16,16 @@ class User < ActiveRecord::Base
   attr_accessible :name, :email, :password, :password_confirmation
 
   has_many :microposts, :dependent => :destroy
+  has_many :relationships, :foreign_key => "follower_id",
+	                   :dependent => :destroy
+  has_many :reverse_relationships, :foreign_key => "followed_id",
+                                   :class_name => "Relationship",
+				   :dependent => :destroy
+
+  has_many :following, :through => :relationships, :source => :followed
+  has_many :followers, :through => :reverse_relationships,
+                       :source  => :follower
+
 
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
@@ -34,7 +44,8 @@ class User < ActiveRecord::Base
 
   def feed
     # This is preliminary. See Chp 12
-    Micropost.where("user_id = ?", id)
+    #Micropost.where("user_id = ?", id)
+    Micropost.from_users_followed_by(self)
   end
 
   # Return true if the user's password matches the submitted password.
@@ -42,6 +53,18 @@ class User < ActiveRecord::Base
     # Compare encrypted_password with the encrypted version of
     # submitted_password.
     encrypted_password == encrypt(submitted_password)
+  end
+
+  def following?(followed)
+	  relationships.find_by_followed_id(followed)
+  end
+
+  def follow!(followed)
+	  relationships.create!(:followed_id => followed.id)
+  end
+
+  def unfollow!(followed)
+	  relationships.find_by_followed_id(followed).destroy
   end
 
   def self.authenticate(email, submitted_password)
